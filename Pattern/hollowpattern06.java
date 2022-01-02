04da99366feeadf7e25078fecda34b8ee4903c94
Debabrata90709 <zsdfgxdfgh@gmail.com>
2022-01-02 21:30:39 +0530

    change

--- Pattern/hollowpattern06.java
+++ Pattern/hollowpattern06.java
@@ -1,18 +1,15 @@
 import java.util.*;
-public class pattern07 {
+public class hollowpattern06 {
     public static void main(String args[]){
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter a number");
     int n = sc.nextInt();
     for(int i=1;i<=n;i++){
-    for (int j=1;j<=i;j++){
+    for (int j=i;j<=n;j++){
+        if(i==1||j==i||j==n)
+        System.out.print("* ");
+        else
         System.out.print("  ");
-    }
-    for (int j=i; j<=n;j++){
-        System.out.print("* ");
-    }
-    for (int j=i;j<n;j++){
-        System.out.print("* ");
     }
 System.out.println();
 
@@ -20,12 +17,15 @@ System.out.println();
 }
 }
 
+
 /*
 
-  * * * * * * * * * 
-    * * * * * * *
-      * * * * *
-        * * *
-          *
+* * * * * * * 
+*         *
+*       *
+*     *
+*   *
+* *
+*
 
 */

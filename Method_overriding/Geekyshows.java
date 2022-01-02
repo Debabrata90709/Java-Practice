22998d722d19b2d4c831813f75f5ecda2e965eed
Debabrata90709 <zsdfgxdfgh@gmail.com>
2022-01-02 16:37:34 +0530

    change

--- Method_overriding/Geekyshows.java
+++ Method_overriding/Geekyshows.java
@@ -1,1 +1,38 @@
-class Father
+class Geekyshows
+{
+    public static void main(String args[])
+    {
+
+    }
+}
+
+class Father        //super class
+{
+    int a = 100;
+    void cal()      //Method
+    {
+        System.out.println("Super class Father\n");
+    }
+}
+
+class Son extends Father    //sub class son
+{
+    int b = 200;
+    void cal()      //Method overriding
+    {
+        int sum = a + b;
+        System.out.println("Sub Class Son");
+        System.out.println("Addition = "+sum);
+    }
+}
+
+class Daughter extends Father       //sub class Daughter
+{
+    int c = 20;
+    void cal()      //Method Overriding
+    {
+        int sum = a + b + c;
+        System.out.println("Sub Class Daughter");
+        System.out.println("Addition = "+sum);
+    }
+}

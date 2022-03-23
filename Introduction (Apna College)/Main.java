2fa8167167c85bb54e99afcb452cc17068f1aff2
Debabrata90709 <zsdfgxdfgh@gmail.com>
2022-03-23 23:51:51 +0530

    3 commit on 23 march 2022

---
+++ Introduction (Apna College)/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World With Java");
+        System.out.println("Hello World With Java");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println(name);
+
+    }
+}

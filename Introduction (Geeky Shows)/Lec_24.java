a0e742bde38479e4bdb5fc855f7fd0d22639b645
Debabrata90709 <zsdfgxdfgh@gmail.com>
2022-03-23 22:51:39 +0530

    commit on 23 march 2022

---
+++ Introduction (Geeky Shows)/Lec_24.java
@@ -0,0 +1,9 @@
+class Lec_24
+{
+    public static void main (String args[])
+    {
+        int total=101;
+        System.out.println("Geeky Shows");
+        System.out.println("Value = "+total);
+    }
+}
---
+++ Introduction (Geeky Shows)/Lec_25.java
@@ -0,0 +1,16 @@
+class Student
+{
+    int roll=1;
+    int marks=1;
+    void disp ()
+    {
+        System.out.println("Roll: "+roll);
+        System.out.println("Marks: "+marks);
+    }
+} 
+public class Lec_25 {
+    public static void main (String args[]){
+        Student Student1 = new Student();
+      
+    }
+}

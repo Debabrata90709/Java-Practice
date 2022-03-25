c16ae640ffd1838e5be1d90af5b45054b2bddc51
Debabrata90709 <zsdfgxdfgh@gmail.com>
2022-03-25 22:38:41 +0530

    commit

---
+++ Introduction (Apna College)/OOPS/OOPS2.java
@@ -0,0 +1,26 @@
+class Student2 {
+    String name;
+    int age;
+
+    public void printInfo(String name) {
+        System.out.println(name);
+    }
+
+    public void printInfo(int age) {
+        System.out.println(age);
+    }
+
+    public void printInfo(String name, int age) {
+        System.out.println(name + " " + age);
+    }
+}
+
+public class OOPS2 {
+    public static void main(String args[]) {
+        Student2 s1 = new Student2();
+        s1.name = "aman";
+        s1.age = 24;
+
+        s1.printInfo(s1.name, s1.age);
+    }
+}
